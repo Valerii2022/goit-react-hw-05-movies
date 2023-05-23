bc45fdcd66a3dfe5c0a1696954c35eac5d1c51c7
Valerii Pometun <vmpometun@gmail.com>
2023-05-23 09:58:21 +0300

    start HW05

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
+
+export const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/About" element={<div>About</div>} />
+        <Route path="/Images" element={<div>Images</div>} />
+      </Routes>
+    </div>
+  );
+};
