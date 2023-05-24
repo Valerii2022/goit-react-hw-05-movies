3085337f30296e73ce5615899d3d1edbaf9199f5
Valerii Pometun <vmpometun@gmail.com>
2023-05-25 00:31:32 +0300

    add lazy load and suspense

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState, useRef, Suspense } from 'react';
 import {
   NavLink,
   Outlet,
@@ -57,7 +57,9 @@ const MovieDetails = () => {
               <NavLink to={`/movies/${movieId}/reviews`}>Reviews</NavLink>
             </li>
           </ul>
-          <Outlet />
+          <Suspense fallback={<div>Loading...</div>}>
+            <Outlet />
+          </Suspense>
         </div>
       )}
     </>
